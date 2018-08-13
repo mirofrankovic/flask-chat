87803e58bc3d283958be6e2368ccdec164632c52
Miro Frankovic <miro.frankovic@gmail.com>
2018-08-13 20:32:37 +0000

    Refactor views to use HTML templates and handle POST request from user

--- run.py
+++ run.py
@@ -1,21 +1,28 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime 
+from flask import Flask, redirect, render_template, request 
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    message_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(message_dict)
     
 def get_all_messages():
     """Get all of the messages and separate them by a `br`"""
-    return "<br>".join(messages) 
+    return messages
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n") 
+        return redirect(request.form["username"])    
+    return render_template("index.html")
     
 @app.route('/<username>') 
 def user(username):
